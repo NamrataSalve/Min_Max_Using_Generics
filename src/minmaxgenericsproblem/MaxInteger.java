b7ffd7f6777b5194a5992c9e8820113e1dab51c9
NamrataSalve <namratasalve1999@gmail.com>
2024-10-06 15:25:16 +0530

    [Namrata] Added elcome masage

---
+++ src/minmaxgenericsproblem/MaxInteger.java
@@ -0,0 +1,7 @@
+package minmaxgenericsproblem;
+
+public class MaxInteger {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Min Max Generics problem");
+    }
+}
