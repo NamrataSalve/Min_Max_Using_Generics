01681ea5db4177eb6bd8e80a67613c4b4bf5c01b
NamrataSalve <namratasalve1999@gmail.com>
2024-10-06 15:59:22 +0530

    [Namrata] Added: create generic alss to take in 3 variable of generics types

--- src/minmaxgenericsproblem/MaxString.java
+++ src/minmaxgenericsproblem/MaxString.java
@@ -2,25 +2,19 @@ package minmaxgenericsproblem;
 
 public class MaxString {
     public static void main(String[] args) {
-        Integer[] integerArray = {10, 40, 20};
-        getMax(integerArray);
-        Float[] floatArray = {2.3f,4.2f,7.5f};
-        getMax(floatArray);
-        String[] stringArray = {"Apple", "Peach", "Banana"};
-        getMax(stringArray);
+        getMax(10,40,20);
+        System.out.println("Maximum integer is "+getMax(10,40,20));
+        getMax(2.3f,4.2f,7.5f);
+        System.out.println("Maximum float is "+getMax(2.3f,4.2f,7.5f));
+        getMax("Apple", "Peach", "Banana");
+        System.out.println("Maximum string is "+getMax("Apple", "Peach", "Banana"));
     }
-    public static <T extends Comparable<T>> void getMax(T[] arr) {
-        T max = arr[0];
-        int position = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int a = arr[i].compareTo(max);
-            if (a > 0) {
-                if (a > 0) {
-                    max = arr[i];
-                    position = i;
-                }
-            }
-            System.out.println("Maximum element number is " + max + " and its position is " + position);
-        }
+    public static <T extends Comparable<T>> T getMax(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
+        if (z.compareTo(max) > 0)
+            max = z;
+        return max;
     }
 }
